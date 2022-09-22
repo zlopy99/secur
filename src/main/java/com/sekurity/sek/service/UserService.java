71674225317c2177b6e48084ba7bd4725c3e0496
Vinko-Tino <Zlopasa@hotmail.com>
2022-09-22 19:39:12 +0200

    PasswordEncrypt

--- src/main/java/com/sekurity/sek/service/UserService.java
+++ src/main/java/com/sekurity/sek/service/UserService.java
@@ -1,10 +1,12 @@
 package com.sekurity.sek.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,12 +15,14 @@ import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
+    @Autowired
+    public PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         List<User> users = new ArrayList<>();
-        users.add(new User("admin", "password", Collections.singleton(new SimpleGrantedAuthority("ADMIN"))));
+        users.add(new User("admin", passwordEncoder.encode("password"), Collections.singleton(new SimpleGrantedAuthority("ADMIN"))));
         users.add(new User("user1", "password", Collections.singleton(new SimpleGrantedAuthority("USER"))));
         users.add(new User("user2", "password", Collections.singleton(new SimpleGrantedAuthority("USER"))));
 
